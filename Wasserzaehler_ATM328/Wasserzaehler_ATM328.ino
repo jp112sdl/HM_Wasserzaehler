03d8f9617e9b3a5fcb63e6bb2a3e8574358869cc
jp112sdl <jp112sdl@icloud.com>
2017-06-29 11:49:21 +0200

    minor fixes

--- Wasserzaehler_ATM328/Wasserzaehler_ATM328.ino
+++ Wasserzaehler_ATM328/Wasserzaehler_ATM328.ino
@@ -1,11 +1,14 @@
 #include <SoftwareSerial.h>
-#define LDR 3
+#define LDR A0
 #define RX  6
 #define TX  5
 
-#define TransmitDelaySeconds        30
-#define ReTransmitDeslaySeconds     10
-#define InputPulseDelayMilliSeconds 350
+#define DEBUG                        false
+#define TransmitDelaySeconds         15
+#define ReTransmitDeslaySeconds      10
+#define PulseDelayMilliSeconds       500
+#define LDRHIGH                      540  //Anbau = 250; Haus = 610
+#define LDRLOW                       490   //Anbau = 50; Haus = 550
 
 SoftwareSerial ESP8266(RX, TX);
 
@@ -13,13 +16,16 @@ int cnt = 0;
 int sentCnt = 0;
 bool ack = true;
 unsigned long oldMillis = 0;
-unsigned long cntMillis = 0;
+unsigned long pulseDiffMillis = 0;
 unsigned long timeoutMillis = 0;
+unsigned long debugMillis = 0;
 String incomingStr;
-boolean oldPinState = LOW;
+boolean oldPinState = HIGH;
+boolean LDRstate = LOW;
 
 void setup() {
-  pinMode(LDR, INPUT_PULLUP);
+  pinMode(LDR, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   ESP8266.begin(9600);
   Serial.begin(115200);
   Serial.println("Ready");
@@ -29,29 +35,41 @@ void loop() {
   if (oldMillis > millis())
     oldMillis = 0;
 
-  if (cntMillis > millis())
-    cntMillis = 0;
+  if (pulseDiffMillis > millis())
+    pulseDiffMillis = 0;
 
   if (timeoutMillis > millis())
     timeoutMillis = 0;
 
-  //Eingangs-Pin überwachen (attachInterrupt funktioniert am Arduino Pro mini nicht zuverlässig!)
-  if (digitalRead(LDR) == LOW) {
-    if (oldPinState == HIGH) {
-      //Serial.println("LDR = LOW!");
-      if ((millis() - cntMillis > InputPulseDelayMilliSeconds) ) {
-        cnt++;
-        Serial.println("IMPULS erkannt");
-        Serial.println("Millis Diff = " + String((millis() - cntMillis)));
+  if (debugMillis > millis())
+    timeoutMillis = 0;
+
+  int LDRval = analogRead(LDR);
+  if (LDRval > LDRHIGH) LDRstate = HIGH;
+  if (LDRval < LDRLOW) LDRstate = LOW;
+  digitalWrite(LED_BUILTIN, LDRstate);
+
+  if (LDRstate == HIGH) {
+    if (oldPinState == LOW) {
+      Serial.println("LOW -> HIGH (" + String(LDRval) + ")");
+      if ((millis() - pulseDiffMillis > PulseDelayMilliSeconds) ) {
+        if (!DEBUG) cnt++;
+        Serial.print("IMPULS erkannt ");
+        Serial.println("Millis Diff = " + String((millis() - pulseDiffMillis)));
       }
-      oldPinState = LOW;
+      oldPinState = HIGH;
     }
   } else {
-    if (oldPinState == LOW) {
-      //Serial.println("LDR = HIGH!");
-      oldPinState = HIGH;
-      cntMillis = millis();
+    if (oldPinState == HIGH) {
+      Serial.println("HIGH -> LOW (" + String(LDRval) + ")");
+      oldPinState = LOW;
+      pulseDiffMillis = millis();
     }
+  }
+
+  if (DEBUG && millis() - debugMillis > 100) {
+    debugMillis = millis();
+    Serial.println("Analogwert = " + String(LDRval));
   }
 
   if (millis() - oldMillis > 1000 * TransmitDelaySeconds) {
@@ -81,7 +99,6 @@ void loop() {
       Serial.println("Empfang bestaetigt.");
     }
   }
-  delay(10);
 }
 
 
